2ba5e57d33188aff27cfe8f40069d8ccfdd35095
Ed Mondek <emondek@hotmail.com>
2015-05-27 14:10:41 -0500

    Added new scripts

---
+++ Start-AzureVMs.ps1
@@ -0,0 +1,16 @@
+﻿# Connect to your Azure account (only needed when your token has expired)
+#Add-AzureAccount
+
+# Initialize variables
+$subscriptionName = '<Subscription Name>'
+$servicename = '<Service Name>'
+$vms = '<VM 1>', '<VM 2>', '<VM 3>', '<VM N>'
+
+# Set the current subscription
+Select-AzureSubscription -SubscriptionName $subscriptionName
+
+# Virtual Machines -> start
+foreach ($vm in $vms)
+{
+    Start-AzureVM -Name $vm -ServiceName $servicename
+}
---
+++ Stop-AzureVMs.ps1
@@ -0,0 +1,16 @@
+﻿# Connect to your Azure account (only needed when your token has expired)
+#Add-AzureAccount
+
+# Initialize variables
+$subscriptionName = '<Subscription Name>'
+$servicename = '<Service Name>'
+$vms = '<VM 1>', '<VM 2>', '<VM 3>', '<VM N>'
+ 
+# Set the current subscription
+Select-AzureSubscription -SubscriptionName $subscriptionName
+
+# Virtual Machines -> shutdown and de-provision
+foreach ($vm in $vms)
+{
+    Stop-AzureVM -ServiceName $servicename -Name $vm -Force
+}
